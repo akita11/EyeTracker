f0c120a7cfd3aee34562e2752ac403ba60b735f5
Koji Ishiwatari <koji.ishiwatari@gmail.com>
2017-04-15 23:34:19 +0900

    Remove space code.

--- designs/sim/teshbench/SIM_TOP.v
+++ designs/sim/teshbench/SIM_TOP.v
@@ -129,12 +129,16 @@ module  SIM_TOP();
         $finish;
     end
 
-    always #(`CLOCK_PERIOD/2) begin
-        clk <=  ~clk;
+    always begin
+        clk = 1'b1;
+        #(`CLOCK_PERIOD/2)  clk = 1'b0;
+        #(`CLOCK_PERIOD/2);
     end
 
-    always #(`CCLOCK_PERIOD/2) begin
-        cclk <=  ~cclk;
+    always begin
+        cclk = 1'b1;
+        #(`CCLOCK_PERIOD/2) cclk = 1'b0;
+        #(`CCLOCK_PERIOD/2);
     end
 
 endmodule
