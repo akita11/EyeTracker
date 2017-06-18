1193093110a9f5321459dc16a49dc3999825aadf
Koji Ishiwatari <koji.ishiwatari@gmail.com>
2017-06-18 18:46:12 +0900

    Update DIV_28_20 script file.

--- designs/src/tcl/generate_IP_design_DIV_28_20.tcl
+++ designs/src/tcl/generate_IP_design_DIV_28_20.tcl
@@ -15,16 +15,15 @@
 create_ip -name div_gen -vendor xilinx.com -library ip -version 5.1 -module_name DIV_28_20 \
           -dir c:/work/EyeTracker/designs/FPGA/project_1/project_1.srcs/sources_1/ip
 set_property -dict [list CONFIG.dividend_and_quotient_width {28} CONFIG.divisor_width {20} \
-             CONFIG.operand_sign {Unsigned} CONFIG.ACLKEN {true} CONFIG.ARESETN {true} \
-             CONFIG.fractional_width {20} CONFIG.FlowControl {Blocking} \
-             CONFIG.OutTready {true} CONFIG.latency {33}] [get_ips DIV_28_20]
+             CONFIG.operand_sign {Unsigned} CONFIG.divide_by_zero_detect {true} CONFIG.ACLKEN {true} CONFIG.ARESETN {true} \
+             CONFIG.fractional_width {20} CONFIG.latency {30}] [get_ips DIV_28_20]
 #generate_target {instantiation_template} [get_files c:/work/EyeTracker/designs/FPGA/project_1/project_1.srcs/sources_1/ip/DIV_28_20/DIV_28_20.xci]
 generate_target all [get_files  c:/work/EyeTracker/designs/FPGA/project_1/project_1.srcs/sources_1/ip/DIV_28_20/DIV_28_20.xci]
-#catch { config_ip_cache -export [get_ips -all DIV_28_20] }
+catch { config_ip_cache -export [get_ips -all DIV_28_20] }
 export_ip_user_files -of_objects [get_files c:/work/EyeTracker/designs/FPGA/project_1/project_1.srcs/sources_1/ip/DIV_28_20/DIV_28_20.xci] \
                      -no_script -sync -force -quiet
 create_ip_run [get_files -of_objects [get_fileset sources_1] c:/work/EyeTracker/designs/FPGA/project_1/project_1.srcs/sources_1/ip/DIV_28_20/DIV_28_20.xci]
-#launch_runs -jobs 2 DIV_28_20_synth_1
+launch_runs -jobs 2 DIV_28_20_synth_1
 export_simulation -of_objects [get_files c:/work/EyeTracker/designs/FPGA/project_1/project_1.srcs/sources_1/ip/DIV_28_20/DIV_28_20.xci] \
                   -directory C:/work/EyeTracker/designs/FPGA/project_1/project_1.ip_user_files/sim_scripts \
                   -ip_user_files_dir C:/work/EyeTracker/designs/FPGA/project_1/project_1.ip_user_files \
