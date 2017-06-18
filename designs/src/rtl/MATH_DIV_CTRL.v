8594fe1dfc8ab8a12613b5d5dc76341c92617107
Koji Ishiwatari <koji.ishiwatari@gmail.com>
2017-06-18 19:29:49 +0900

    Support divider function for SX/S and SY/S.

--- designs/src/rtl/MATH_DIV_CTRL.v
+++ designs/src/rtl/MATH_DIV_CTRL.v
@@ -19,12 +19,16 @@ module MATH_DIV_CTRL #(
     input   wire                                CLK,
     input   wire                                RST_N,
     //
+    input                                       iTRIG,
+    //
     input   wire                                iDIVIDEND_TREADY,
     input   wire                                iDIVISOR_TREADY,
     input   wire                                iDOUT_TVALID,
     //
     output  wire                                oDIVIDEND_TVALID,
-    output  wire                                oDIVISOR_TVALID
+    output  wire                                oDIVISOR_TVALID,
+    //
+    output  wire                                oDIVID_LOAD_EN
 );
 
     // 
@@ -37,22 +41,24 @@ module MATH_DIV_CTRL #(
     localparam      STATE_WIDTH                 = 4;
 
     //
-    wire                                        start_trig;
+    localparam      COUNTER_WIDTH               = 8;
 
-    wire                                        load_en;
+    //
+    wire                                        start_trig;
 
     //
     reg     [STATE_WIDTH -1: 0]                 next_state, state;
     reg                                         next_dividend_tvalid, dividend_tvalid;
     reg                                         next_divisor_tvalid, divisor_tvalid;
-//  reg                                         next_divid_load_en, divid_load_en;
+    reg                                         next_divid_load_en, divid_load_en;
 
     //
     assign  oDIVIDEND_TVALID = dividend_tvalid;
     assign  oDIVISOR_TVALID  = divisor_tvalid;
+    assign  oDIVID_LOAD_EN   = divid_load_en;
 
     // 
-    DET_EDGE m_DET_START_TRIG( .CLK(CLK), .RST_N(RST_N), .iS(iDE), .oRISE(start_trig), .oFALL() );
+    DET_EDGE m_DET_START_TRIG( .CLK(CLK), .RST_N(RST_N), .iS(iTRIG), .oRISE(start_trig), .oFALL() );
 
     //
 //    DFF_REG #( .DATA_WIDTH(8), .INIT_VAL(8'h00) ) m_DIVIDED_SX_L( .CLK(CLK), .RST_N(RST_N), .iWE(load_en), .iD(iDATA), .oD(DIVIDED_SX[ 7: 0]) );
@@ -61,9 +67,6 @@ module MATH_DIV_CTRL #(
     //
 //    DFF_REG #( .DATA_WIDTH(8), .INIT_VAL(8'h00) ) m_DIVIDED_SY_L( .CLK(CLK), .RST_N(RST_N), .iWE(load_en), .iD(iDATA), .oD(oDIVIDED_SY[ 7: 0]) );
 //    DFF_REG #( .DATA_WIDTH(8), .INIT_VAL(8'h00) ) m_DIVIDED_SY_H( .CLK(CLK), .RST_N(RST_N), .iWE(load_en), .iD(iDATA), .oD(oDIVIDED_SY[15: 8]) );
-
-    //
-    assign  load_en = iDIVIDEND_TREADY & iDIVISOR_TREADY & iDOUT_TVALID;
 
     //
     always @(*) begin
@@ -76,42 +79,53 @@ module MATH_DIV_CTRL #(
                                 //
                                 next_divisor_tvalid  <= 1'b0;
                                 //
+                                next_divid_load_en   <= 'h0;
                             end else begin
                                 next_state           <= state;
                                 //
                                 next_dividend_tvalid <= dividend_tvalid;
                                 //
                                 next_divisor_tvalid  <= divisor_tvalid;
+                                //
+                                next_divid_load_en   <= divid_load_en;
                             end
                         end
             SET_DIVIDEND:begin
                             if (iDIVIDEND_TREADY) begin
                                 next_state           <= SET_DIVISOR;
                                 //
-                                next_dividend_tvalid <= 1'b0;
+                                next_dividend_tvalid <= dividend_tvalid;
                                 //
                                 next_divisor_tvalid  <= 1'b1;
+                                //
+                                next_divid_load_en   <= 'h0;
                             end else begin
                                 next_state           <= state;
                                 //
                                 next_dividend_tvalid <= dividend_tvalid;
                                 //
                                 next_divisor_tvalid  <= divisor_tvalid;
+                                //
+                                next_divid_load_en   <= divid_load_en;
                             end
                         end
             SET_DIVISOR:begin
-                            if (load_en) begin
-                                next_state           <= SET_DIVISOR;
+                            if (iDOUT_TVALID==1'b1) begin
+                                next_state           <= CALC_DIV_OUT;
                                 //
                                 next_dividend_tvalid <= 1'b0;
                                 //
-                                next_divisor_tvalid  <= 1'b1;
+                                next_divisor_tvalid  <= 1'b0;
+                                //
+                                next_divid_load_en   <= 'h1;
                             end else begin
                                 next_state           <= state;
                                 //
                                 next_dividend_tvalid <= dividend_tvalid;
                                 //
                                 next_divisor_tvalid  <= divisor_tvalid;
+                                //
+                                next_divid_load_en   <= divid_load_en;
                             end
                         end
             CALC_DIV_OUT:begin
@@ -120,6 +134,8 @@ module MATH_DIV_CTRL #(
                             next_dividend_tvalid <= 1'b0;
                             //
                             next_divisor_tvalid  <= 1'b0;
+                            //
+                            next_divid_load_en   <= 'h0;
                         end
             default:    begin
                             next_state           <= IDLE_STATE;
@@ -127,6 +143,8 @@ module MATH_DIV_CTRL #(
                             next_dividend_tvalid <= 1'b0;
                             //
                             next_divisor_tvalid  <= 1'b0;
+                            //
+                            next_divid_load_en   <= 'h0;
                         end
         endcase
     end
@@ -139,12 +157,16 @@ module MATH_DIV_CTRL #(
             dividend_tvalid <= 1'b0;
             //
             divisor_tvalid  <= 1'b0;
+            //
+            divid_load_en   <= 'h0;
         end else begin
             state           <= next_state;
             //
             dividend_tvalid <= next_dividend_tvalid;
             //
             divisor_tvalid  <= next_divisor_tvalid;
+            //
+            divid_load_en   <= next_divid_load_en;
         end
     end
 
