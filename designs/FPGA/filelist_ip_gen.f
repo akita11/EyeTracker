488b1f94d744fe4710c2ee894de65d2809c3fdf0
Koji Ishiwatari <koji.ishiwatari@gmail.com>
2017-06-24 10:17:27 +0900

    Update filelist

--- designs/FPGA/filelist_ip_gen.f
+++ designs/FPGA/filelist_ip_gen.f
@@ -1,3 +1,4 @@
-../src/tcl/generate_IP_design_CLKGEN_MMCM.tcl
-../src/tcl/generate_IP_design_MEM.tcl
-../src/tcl/generate_IP_design_ILA.tcl
+../src/tcl/generate_IP_design_CLKGEN_MMCM_batch.tcl
+../src/tcl/generate_IP_design_MEM_batch.tcl
+../src/tcl/generate_IP_design_ILA_batch.tcl
+../src/tcl/generate_IP_design_DIV_28_20_batch.tcl
