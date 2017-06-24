e84cb94d2f873755784d1bc0b0e65f7c7e091d53
Koji Ishiwatari <koji.ishiwatari@gmail.com>
2017-06-24 13:43:51 +0900

    Add new file to support generating IP models by cli(tcl).

---
+++ designs/src/tcl/generate_IP_design_DIV_28_20_batch.tcl
@@ -0,0 +1,20 @@
+# // -----------------------------------------------------------------------------
+# //  Title         : Generate IP design for DIV_28_20
+# //  Project       : EyeTracker
+# // -----------------------------------------------------------------------------
+# //  File          : generate_IP_design_DIV_28_20.tcl
+# //  Author        : K.Ishiwatari
+# //  Created       : 2017/ 6/14
+# //  Last modified : 
+# // -----------------------------------------------------------------------------
+# //  Description   : Xilinx Design Constraint file 
+# // -----------------------------------------------------------------------------
+# //  Copyright (C) 2017 K.Ishiwatari All Rights Reserved.
+# // -----------------------------------------------------------------------------
+
+create_ip -name div_gen -vendor xilinx.com -library ip -version 5.1 -module_name DIV_28_20
+set_property -dict [list CONFIG.dividend_and_quotient_width {28} CONFIG.divisor_width {20} \
+             CONFIG.operand_sign {Unsigned} CONFIG.divide_by_zero_detect {true} CONFIG.ACLKEN {true} CONFIG.ARESETN {true} \
+             CONFIG.fractional_width {20} CONFIG.latency {30}] [get_ips DIV_28_20]
+generate_target all [get_ips DIV_28_20] -force
+synth_ip [get_ips DIV_28_20]
---
+++ designs/src/tcl/generate_IP_design_MEM_batch.tcl
@@ -0,0 +1,23 @@
+# // -----------------------------------------------------------------------------
+# //  Title         : Generate IP design for MEM
+# //  Project       : EyeTracker
+# // -----------------------------------------------------------------------------
+# //  File          : generate_IP_design_MEM.tcl
+# //  Author        : K.Ishiwatari
+# //  Created       : 2017/ 1/ 1
+# //  Last modified : 
+# // -----------------------------------------------------------------------------
+# //  Description   : Xilinx Design Constraint file 
+# // -----------------------------------------------------------------------------
+# //  Copyright (C) 2017 K.Ishiwatari All Rights Reserved.
+# // -----------------------------------------------------------------------------
+
+create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.3 -module_name MEM
+set_property -dict [list CONFIG.Memory_Type {True_Dual_Port_RAM} \
+             CONFIG.Algorithm {Low_Power} \
+             CONFIG.Write_Width_A {640} CONFIG.Write_Depth_A {512} CONFIG.Read_Width_A {640} CONFIG.Operating_Mode_A {NO_CHANGE} \
+             CONFIG.Write_Width_B {640} CONFIG.Read_Width_B {640} CONFIG.Operating_Mode_B {NO_CHANGE} CONFIG.Enable_B {Use_ENB_Pin} \
+             CONFIG.Register_PortA_Output_of_Memory_Primitives {true} CONFIG.Register_PortB_Output_of_Memory_Primitives {true} \
+             CONFIG.Port_B_Clock {100} CONFIG.Port_B_Write_Rate {50} CONFIG.Port_B_Enable_Rate {100}] [get_ips MEM]
+generate_target all [get_ips MEM] -force
+synth_ip [get_ips MEM]
