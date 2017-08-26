aa4d29f4b36fcbf0c641c023b4d342086b84045c
Koji Ishiwatari <koji.ishiwatari@gmail.com>
2017-08-26 11:53:19 +0900

    Modify shell script for Jenkins

--- designs/FPGA/run_generate_bitstream.sh
+++ designs/FPGA/run_generate_bitstream.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
+
+##### Get work directory
+work_dir=`dirname $0`
+
+##### Change current directory
+cd ${work_dir}
+
+##### Vivado version
+vivado_ver=2016.4
+
+##### Set Vivado environment
+source /apps/Xilinx/Vivado${vivado_ver}/Vivado/${vivado_ver}/settings64.sh
+
+##### 
 vivado -mode batch -source ./run_generate_bitstream.tcl
